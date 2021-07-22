1756a9625ecc4a977f1780ef5f21aef69c93c958
juankosasih97 <juan.kosasih@gmail.com>
2021-07-22 22:46:58 +0800

    Update travis to run cypress tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import RootReducer from './reducers/RootReducer';
+
+const store = createStore(RootReducer);
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>);
+  const linkElement = screen.getByText(/DEMO Streaming/i);
   expect(linkElement).toBeInTheDocument();
 });
